e33a66b02a946f264de44f0b2644c950032b3891
Similien <ksimilien@deloitte.ca>
2017-08-20 16:45:53 -0400

    Add project files.

---
+++ helloworld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("HelloWorld");
+           Console.ReadLine();
+        }
+    }
+}
+
+
