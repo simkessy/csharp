f591330c184ac2265c0017f73df0983a35b04718
simkessy@gmail.com <gitNaruto10!>
2017-08-20 18:02:03 -0400

    Variables done

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -10,6 +10,25 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            // Variables lesson 
+            //int x = 7;
+            //int y = x + 3;
+            //Console.WriteLine(y);
+
+            //string firstname = "Kessy";
+            //Console.WriteLine(firstname);
+
+            int x = 7;
+            //string y = "kc";
+            string y = "5";
+            string firstTry = x.ToString() + y;
+
+            //int secondTry = x + y;
+            int secondTry = x + int.Parse(y);
+
+            //Console.WriteLine(firstTry);
+            Console.Write(secondTry);
+            Console.ReadLine();
         }
     }
 }
